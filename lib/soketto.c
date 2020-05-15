c118238e6e39bda22821eea3912e4aad4265ceb4
Sammy Hajhamid <pokeso123@hotmail.com>
2020-05-15 05:08:11 -0700

    added recv callbacks and error handling for many things, fixed up sending and added threads

--- lib/soketto.c
+++ lib/soketto.c
@@ -1,77 +1,116 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #define BAD_SOCK_FD -1
 #define BAD_ADDRESS -2
 #define CONN_ERR -3
 
-enum PacketTypes {
+enum PacketTypes
+{
     DataPacket = 0,
     ClosePacket,
 };
 
-
-struct sokettoClientFunctionTable {
-    ssize_t (*send)(skClient *self, void *buf, size_t len);
-};
-
-const struct sokettoClientFunctionTable ft = (struct sokettoClientFunctionTable) {
-    .send = sokettoSend,
-};
-
-typedef struct sokettoClientConnection {
-    uint16_t sock_fd;
+typedef struct sokettoClientConnection
+{
+    int sock_fd;
+    pthread_t thread;
     struct sokettoClientFunctionTable *ft;
+    void (*recvCallback)(void *data, ssize_t data_amount);
 } skClient;
 
-typedef struct sokettoPacketHeader {
+typedef struct sokettoPacketHeader
+{
     uint32_t packet_size;
     uint8_t packet_id;
     uint8_t data[];
 } skPacket;
 
-skClient *skClientConnect(uint16_t port, char *address, int8_t *err) {
-    uint16_t sock_fd;
+ssize_t sokettoSend(skClient *self, void *buf, size_t len)
+{
+    skPacket packet;
+    packet.packet_size = (uint32_t)len;
+    packet.packet_id = DataPacket;
+    return send(self->sock_fd, (void *)&packet, sizeof(skPacket), 0) + send(self->sock_fd, buf, len, 0);
+}
+
+int sokettoClose(skClient *self)
+{
+    skPacket packet;
+    packet.packet_size = 0;
+    packet.packet_id = ClosePacket;
+    if (send(self->sock_fd, &packet, sizeof(skPacket), 0) < 0)
+    {
+        return -1;
+    }
+    return close(self->sock_fd);
+}
+
+void *sokettoRecvThread(skClient *self)
+{
+    skPacket header;
+    for (;;)
+    {
+        if (self->recvCallback)
+        {
+            memset(&header, 0, sizeof(skPacket));
+            if (recv(self->sock_fd, &header, sizeof(skPacket), 0) != sizeof(skPacket))
+                continue;
+            void *data_chunk = malloc(header.packet_size);
+            ssize_t data_size = recv(self->sock_fd, data_chunk, header.packet_size, 0);
+            if (self->recvCallback)
+                self->recvCallback(data_chunk, data_size);
+            free(data_chunk);
+        }
+    }
+}
+
+struct sokettoClientFunctionTable
+{
+    ssize_t (*send)(skClient *self, void *buf, size_t len);
+};
+
+struct sokettoClientFunctionTable ft = (struct sokettoClientFunctionTable){
+    .send = sokettoSend,
+};
+
+skClient *skClientConnect(char *address, uint16_t port, int8_t *err)
+{
+    int sock_fd;
     struct sockaddr_in sock_address;
-    if (sock_fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
-        if (err) *err = BAD_SOCK_FD;
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        if (err)
+            *err = BAD_SOCK_FD;
         return NULL;
     }
     sock_address.sin_family = AF_INET;
     sock_address.sin_port = htons(port);
-    if (inet_pton(AF_INET, address, &sock_address.sin_addr) <= 0) {
-        if (err) *err = BAD_ADDRESS;
+    if (inet_pton(AF_INET, address, &sock_address.sin_addr) <= 0)
+    {
+        if (err)
+            *err = BAD_ADDRESS;
         return NULL;
     }
-    if (connect(sock_fd, (struct sockaddr *) &sock_address, sizeof(sock_address) < 0)) {
-        if (err) *err = CONN_ERR;
+    if (connect(sock_fd, (struct sockaddr *)&sock_address, sizeof(sock_address)) != 0)
+    {
+        if (err)
+            *err = CONN_ERR;
         return NULL;
     }
     skClient *sk_client = malloc(sizeof(skClient));
-    *sk_client = (skClient) {
+    *sk_client = (skClient){
         .sock_fd = sock_fd,
         .ft = &ft,
+        .thread = 0,
     };
+    pthread_create(&sk_client->thread, NULL, sokettoRecvThread, sk_client);
     return sk_client;
-}
-
-ssize_t sokettoSend(skClient *self, void *buf, size_t len) {
-    skPacket packet;
-    packet.packet_size = (uint32_t) len;
-    packet.packet_id = DataPacket;
-    return send(self->sock_fd, (void *) &packet, sizeof(skPacket), 0) + send(self->sock_fd, buf, len, 0);
-}
-
-int sokettoClose(skClient *self) {
-    skPacket packet;
-    packet.packet_size = 0;
-    packet.packet_id = ClosePacket;
-    if (send(self->sock_fd, &packet, sizeof(skPacket), 0) < 0) {
-        return -1;
-    }
-    return close(self->sock_fd);
 }
--- tests/echo_client.c
+++ tests/echo_client.c
@@ -1,1 +1,21 @@
-#include <
+#include "../lib/soketto.c"
+
+void recvCallback(void *_data, ssize_t size)
+{
+    char *data = (char *)_data;
+    data[size - 1] = '\0';
+    printf("data moment: %s", data);
+}
+
+int main()
+{
+    int8_t err;
+    skClient *sock = skClientConnect("127.0.0.1", 2222, &err);
+    if (!sock)
+    {
+        return -1;
+    }
+    sock->ft->send(sock, "owo", 3);
+    sock->recvCallback = recvCallback;
+    for (;;);
+}
